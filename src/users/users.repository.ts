0855aa52b979df99b3739c957c71df105a784402
MohsenKhashei <m.khasheii@gmail.com>
2023-05-27 13:19:46 +0300

    adding user repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
 
 @Injectable()
 export class UserRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  findOne(userFilerQuery: FilterQuery<User>): Promise<User> {
+    return this.userModel.findOne(userFilerQuery);
+  }
+  find(userFilerQuery: FilterQuery<User>): Promise<User[]> {
+    return this.userModel.find(userFilerQuery);
+  }
+  async create(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+  async findOneAndUpdate(
+    userFilterQuery: FilterQuery<User>,
+    user: Partial<User>,
+  ): Promise<User> {
+    return this.userModel.findOneAndUpdate(userFilterQuery, user);
+  }
 }
