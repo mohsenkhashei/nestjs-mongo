1ae108550c6560daecff4544f304c9f2571de553
MohsenKhashei <m.khasheii@gmail.com>
2023-05-27 13:20:23 +0300

    creating schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,6 +3,10 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
+// by default NestJS will automaticlly managed and create the collection asigned to a given schema,
+// it'll create singular user we create here and manageing user collection
+// if you want to overwrite that behaviour pass the object to schema directory @Schema({ collection: 'asdf'})
+
 @Schema()
 export class User {
   @Prop()
